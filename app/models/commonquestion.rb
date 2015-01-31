147aad96b8f694daec3237fa3754b29cf3c54cc5
Dylan Ham <dylanham@gmail.com>
2015-01-31 11:41:33 -0700

    user should see FAQ page

---
+++ app/models/commonquestion.rb
@@ -0,0 +1,11 @@
+class CommonQuestion
+  def Initialize(question,answer,slug)
+    @question = question
+    @answer = answer
+    @slug = slug
+    if @slug == "How much does it cost"
+      return "How-much-does-it-cost"
+    else
+    end
+  end
+end
