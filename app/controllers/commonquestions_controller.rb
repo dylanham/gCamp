21229eb78adae3708af8daabd6f1c5f9a10fef27
Dylan Ham <dylanham@gmail.com>
2015-02-01 16:22:19 -0700

    user can see array output to page

--- app/controllers/commonquestions_controller.rb
+++ app/controllers/commonquestions_controller.rb
@@ -1,7 +1,7 @@
 class CommonquestionsController<ApplicationController
 
   def index
-
+    @questions = CommonQuestion.get_question
   end
 
 end
---
+++ app/models/common_question.rb
@@ -0,0 +1,7 @@
+
+class CommonQuestion
+
+  def self.get_question
+    ['What is gCamp?','How do I join gCamp?','When will gCamp be finished?']
+  end
+end
