b24232b41a8dd6aa0d5b213afc7e36bb75231fba
Dylan Ham <dylanham@gmail.com>
2015-02-02 21:29:54 -0700

    programmer can see better variable usage for faq index

--- app/controllers/common_questions_controller.rb
+++ app/controllers/common_questions_controller.rb
@@ -3,7 +3,7 @@ class CommonQuestionsController<ApplicationController
   def index
     answers = CommonQuestion.get_answer
     slugs = CommonQuestion.get_slug
-    @array=[answers,slugs]
+    @faqs=[slugs,answers]
   end
 
 end
