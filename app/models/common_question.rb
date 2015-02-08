48c8717fb0c028cec540b34941fa719bd88b1b08
Dylan Ham <dylanham@gmail.com>
2015-02-08 13:19:24 -0700

    user can make a new item and edit that item

--- app/models/common_question.rb
+++ app/models/common_question.rb
@@ -2,18 +2,18 @@
 class CommonQuestion
 
   @questions_answers={
-      'What is gCamp?' =>  'gCamp is an awesome tool that is going to change your life.
-      gCamp is your one stop shop to organize all your tasks.
-      You\'ll also be able to track comments that you and others make.
-      gCamp may eventually replace all need for paper and pens in the entire world.
-      Well, maybe not, but it\'s going to be pretty cool.',
-      'How do I join gCamp?'=>  'As soon as it\'s ready for the public,
-      you\'ll see a signup link in the upper right.
-      Once that\'s there, just click it and fill in the form!',
-      'When will gCamp be finished?'  =>  'gCamp is a work in progress.
-      That being said, it should be fully functional in the next few weeks.
-      Functional. Check in daily for new features and awesome functionality.
-      It\'s going to blow your mind. Organization is just a click away. Amazing!'}
+      'What is gCamp?' => 'gCamp is an awesome tool that is going to change your life.
+        gCamp is your one stop shop to organize all your tasks.
+        You\'ll also be able to track comments that you and others make.
+        gCamp may eventually replace all need for paper and pens in the entire world.
+        Well, maybe not, but it\'s going to be pretty cool.',
+      'How do I join gCamp?'=> 'As soon as it\'s ready for the public,
+        you\'ll see a signup link in the upper right.
+        Once that\'s there, just click it and fill in the form!',
+      'When will gCamp be finished?'=> 'gCamp is a work in progress.
+        That being said, it should be fully functional in the next few weeks.
+        Functional. Check in daily for new features and awesome functionality.
+        It\'s going to blow your mind. Organization is just a click away. Amazing!'}
 
 
   def self.get_question_answer
@@ -29,6 +29,6 @@ class CommonQuestion
     question_slug
   end
 
-  
+
 
 end
