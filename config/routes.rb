275a68bacf2151a4fe1717ebde5f176ad384301c
Dylan Ham <dylanham@gmail.com>
2015-02-12 16:15:48 -0700

    User can see users index

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'faq/'  , to: 'common_questions#index'
 
   resources :tasks
-
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
