e44deeba43a82012a4066f535f6c8ca4bee78e01
Dylan Ham <dylanham@gmail.com>
2015-02-13 13:21:33 -0700

    removed uneeded links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -19,30 +18,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to users_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @user.destroy
-    redirect_to users_path
-  end
-
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'faq/'  , to: 'common_questions#index'
 
   resources :tasks
-  resources :users
+  resources :users, only:[:new, :index, :create]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
