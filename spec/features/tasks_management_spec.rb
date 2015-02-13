8b7658a403a34365a448693713687e7a6e0aa777
Dylan Ham <dylanham@gmail.com>
2015-02-13 15:14:00 -0700

    added rspec

---
+++ spec/features/tasks_management_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+describe 'user should be able to go to index page and see something' do
+  it 'works' do
+    visit users_path
+    save_and_open_page
+    expect(page).to have_content 'Users'
+  end
+  it 'should be able to click the New User Button' do
+    visit users_path
+    save_and_open_page
+    expect(page).to click_button('New User')
+  end
+end
