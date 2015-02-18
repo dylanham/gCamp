e0df0747098ab5dc9eedc39719189fab64050f7a
Dylan Ham <dylanham@gmail.com>
2015-02-18 16:38:21 -0700

    rspec can make a new task

--- spec/features/tasks_management_spec.rb
+++ spec/features/tasks_management_spec.rb
@@ -5,9 +5,15 @@ describe 'user should be able to go to index page and see something' do
     save_and_open_page
     expect(page).to have_content 'Tasks'
   end
-  it 'should be able to click the New Task Button' do
+  it 'should be able to make a new tak' do
     visit tasks_path
+    click_on 'New Task'
+    fill_in 'Description', with: 'Do Work'
+    fill_in 'Due date', with: '09172016'
+    click_on 'Create'
+    within '.breadcrumb' do
+      click_on 'Task'
+    end
     save_and_open_page
-    click_link 'New Task'
   end
 end
