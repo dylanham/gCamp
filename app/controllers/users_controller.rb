2cff1ae6a54df42f02a58bd621e62d8a22b161d5
Dylan Ham <dylanham@gmail.com>
2015-02-17 20:18:13 -0700

    update for users page..user can edit their info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only:[:show,:edit,:update]
   def index
     @users = User.all
   end
@@ -18,11 +18,32 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was succesfully updated"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- spec/features/tasks_management_spec.rb
+++ spec/features/tasks_management_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 describe 'user should be able to go to index page and see something' do
-  it 'works' do
+  it 'checks to see if it can find the word Users' do
     visit users_path
     save_and_open_page
     expect(page).to have_content 'Users'
