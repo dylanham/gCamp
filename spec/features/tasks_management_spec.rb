2352ce5cb81db1f07b65cbdb442fcb1f606892d3
Dylan Ham <dylanham@gmail.com>
2015-02-25 10:35:59 -0700

    add travis to gcamp

--- spec/features/tasks_management_spec.rb
+++ spec/features/tasks_management_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 feature 'user should be able to go to index page and see something' do
   it 'checks to see if it can find the word Users' do
     visit tasks_path
-    save_and_open_page
     expect(page).to have_content 'Tasks'
   end
   it 'should be able to make a new tak' do
@@ -14,7 +13,6 @@ feature 'user should be able to go to index page and see something' do
     within '.breadcrumb' do
     click_on 'Task'
     expect(page).to have_content 'Do Work'
-    save_and_open_page
     end
   end
 end
