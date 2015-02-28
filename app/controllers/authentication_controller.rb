44c64b400ca379707dc00bb53f06fdd9d77c4973
Dylan Ham <dylanham@gmail.com>
2015-02-28 12:31:50 -0700

    updated authentication for gschool

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,21 @@
+class AuthenticationController < ApplicationController
+  def destroy
+    session.clear
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "You have successfully signed in"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # Example of regular route:
-  get 'terms/', to: 'terms#index'
-  get 'about/', to: 'terms#about_page'
-  get 'faq/'  , to: 'common_questions#index'
+  get 'terms/',   to: 'terms#index'
+  get 'about/',   to: 'terms#about_page'
+  get 'faq/'  ,   to: 'common_questions#index'
+  get 'sign-up',  to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  get 'sign-out', to: 'authentication#destroy'
+  get 'sign-in',  to: 'authentication#new'
+  post'sign-in',  to: 'authentication#create'
 
   resources :tasks
   resources :users
