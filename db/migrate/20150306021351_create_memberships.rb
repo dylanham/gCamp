8f04f9cdbce653ede54f1a761a07458587c4fac9
Dylan Ham <dylanham@gmail.com>
2015-03-05 19:18:32 -0700

    add membership model and database table

---
+++ db/migrate/20150306021351_create_memberships.rb
@@ -0,0 +1,11 @@
+class CreateMemberships < ActiveRecord::Migration
+  def change
+    create_table :memberships do |t|
+      t.string :role
+      t.integer :user_id
+      t.integer :projects_id
+
+      t.timestamps null: false
+    end
+  end
+end
