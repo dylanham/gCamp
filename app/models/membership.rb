64175f19f2ba55a4ff8e2dd302d8ea3b530201f4
Dylan Ham <dylanham@gmail.com>
2015-03-11 16:05:49 -0600

    clean whitespace

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,6 @@
 class Membership < ActiveRecord::Base
   ROLES = ['Member', 'Owner']
+
   validates :role, inclusion: ROLES
   validates :user, presence: true
   validates_uniqueness_of :user, scope: :project, message: "has already been added to this project"
