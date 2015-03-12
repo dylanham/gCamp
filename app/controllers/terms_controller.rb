aa31ff438205410c3ad0b0f5cdb244cb49a6907c
Dylan Ham <dylanham@gmail.com>
2015-03-11 19:24:58 -0600

    add objects to terms controller and update about page to show count of objects

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,9 +1,14 @@
 class TermsController < ApplicationController
-  
+
   def index
   end
 
   def about_page
+    @projects = Project.all
+    @users = User.all
+    @comments = Comment.all
+    @memberships = Membership.all
+    @tasks = Task.all
   end
 
 end
