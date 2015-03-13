4988a095efdb8609b4647643ff1d7fa1ce1c631f
Dylan Ham <dylanham@gmail.com>
2015-03-12 22:32:41 -0600

    seed file for rake testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  # some first user last user@example.com password password
+
+100.times do
+  Membership.create!(role: 'Member',project_id: nil, user_id: nil)
+end
+
+100.times do
+  User.create!(
+    first_name: "Some#{rand(100)+1}",
+    last_name: "User#{rand(100)+1}",
+    email: "user#{rand(100)+1}@xample.com",
+    password: 'password'
+  )
+end
+
+100.times do
+  Task.create!(description: "A task",project_id: rand(100)+1)
+end
+
+100.times do
+  Comment.create!(content: "A comment", task_id: rand(100)+1, user_id: rand(100)+1)
+end
+
+100.times do
+  Comment.create!(content: "A comment with a task but no user should be (deleted user)", task_id: 888, user_id: rand(100)+1)
+end
+
+100.times do
+  Task.create!(description: "A Task without a project_id", project_id: nil)
+end
+
+100.times do
+  Comment.create!(content: "A comment without a task", task_id: nil)
+end
