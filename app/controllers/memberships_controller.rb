034ec092ae6de94adfe60883807c59fad49648d4
Dylan Ham <dylanham@gmail.com>
2015-03-21 07:23:00 -0600

    add permissions to memberships controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,7 +1,8 @@
 class MembershipsController < ApplicationController
   before_action :find_and_set_project
-  before_action :ensure_current_user
-
+  before_action :ensure_project_member_or_admin, only:[:index]
+  before_action :ensure_project_owner_or_admin, only:[:update, :destroy]
+  before_action :set_membership_and_verify_more_than_one_owner, only:[:update, :destroy]
 
   def index
     @membership = @project.memberships.new
@@ -11,7 +12,7 @@ class MembershipsController < ApplicationController
     membership = @project.memberships.new(membership_params)
     if membership.save
       flash[:notice] = "#{membership.user.full_name} was successfully added"
-      redirect_to project_memberships_path(membership.project_id)
+      redirect_to project_memberships_path(@project)
     else
       @membership = membership
       render :index
@@ -19,21 +20,22 @@ class MembershipsController < ApplicationController
   end
 
   def update
-    membership = Membership.find(params[:id])
-    if membership.update(membership_params)
-      flash[:notice] = "#{membership.user.full_name} was successfully updated"
-      redirect_to project_memberships_path(membership.project_id)
+    if @membership.update(membership_params)
+      flash[:notice] = "#{@membership.user.full_name} was successfully updated"
+      redirect_to project_memberships_path(@project)
     else
-      @membership = membership
       render :index
     end
   end
 
   def destroy
-    membership = Membership.find(params[:id])
-    membership.destroy
-    flash[:notice] = "#{membership.user.full_name} was successfully removed"
-    redirect_to project_memberships_path(membership.project_id)
+    @membership.destroy
+    flash[:notice] = "#{@membership.user.full_name} was successfully removed"
+    if current_user.id == @membership.user_id
+      redirect_to projects_path
+    else
+      redirect_to project_memberships_path(@project)
+    end
   end
 
   private
@@ -44,6 +46,14 @@ class MembershipsController < ApplicationController
 
   def find_and_set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def set_membership_and_verify_more_than_one_owner
+    @membership = Membership.find(params[:id])
+    if @membership.role == 'Owner' && @project.memberships.where(role: 'Owner').count <= 1
+      flash[:warning] = "Projects must have at least one owner"
+      redirect_to project_memberships_path(@project)
+    end
   end
 
 end
