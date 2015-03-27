ed04364121dc6ab09a84f9e4b3ef0e4d0bdda549
Dylan Ham <dylanham@gmail.com>
2015-03-27 08:49:09 -0600

    bug fix for token

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,9 @@ class User < ActiveRecord::Base
   end
 
   def pivotal_tracker_privacy
-    number_of_stars = self.pivotal_tracker_token.length - 4
-    self.pivotal_tracker_token[0..3] + ('*'*number_of_stars)
+    if self.pivotal_tracker_token
+      number_of_stars = self.pivotal_tracker_token.length - 4
+      self.pivotal_tracker_token[0..3] + ('*' * number_of_stars)
+    end
   end
 end
